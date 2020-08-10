aba5e1118229469f820c5a3b5c05670f7a0f479e
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-08-10 18:18:44 -0400

    writing out test cases

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,14 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("here");
+        runFirstTest();
+    }
+
+    public static void runFirstTest() {
+        CustomPriorityQueue queue = new CustomPriorityQueue(1);
+
+        Item a = new Item(1);
+        queue.add(a);
+        System.out.println(a.getPriority());
     }
 }
