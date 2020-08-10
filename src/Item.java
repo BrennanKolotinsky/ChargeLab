9f6957e2c610374fcc421411022fe1318203cb3c
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-08-10 17:34:25 -0400

    created the item class

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,25 @@
-public class Item {
+public class Item<T> {
+
+    private int priority;
+    private T val; // generic type with any value
+
+    public Item(int p) {
+        this.priority = p;
+    }
+
+    public void setPriority(int p) {
+        this.priority = p;
+    }
+
+    public void setVal(T v) {
+        this.val = v;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public T getVal() {
+        return this.val;
+    }
 }
